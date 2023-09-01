d48f767182ef663041a8ac806680cf50f8cdd447
amrita150 <amritakataria515@gmail.com>
2023-09-01 17:13:58 +0530

    Bubble Sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,14 +1,34 @@
 class BubbleSort{
 
+    //its time complexity is O(n^2) at worst or best case
+    // public static void bubbleSort(int arr[]){
+    //     for(int i = 0 ; i<arr.length ; i++){
+    //         for(int j = 1 ; j < (arr.length - i) ; j++){
+    //             if(arr[j-1]>arr[j]){
+    //                 //swap
+    //                 int temp = arr[j-1];
+    //                 arr[j-1] = arr[j];
+    //                 arr[j] = temp;
+    //             }
+    //         }
+    //     }
+    // }
+
+    //its time complexity at worst case is O(n^2) and best case is O(n)
     public static void bubbleSort(int arr[]){
         for(int i = 0 ; i<arr.length ; i++){
+            boolean flag = false;
             for(int j = 1 ; j < (arr.length - i) ; j++){
                 if(arr[j-1]>arr[j]){
                     //swap
                     int temp = arr[j-1];
                     arr[j-1] = arr[j];
                     arr[j] = temp;
+                    flag = true;
                 }
+            }
+            if(flag==false){
+                break;
             }
         }
     }
