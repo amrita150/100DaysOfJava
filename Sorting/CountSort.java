9c97523fdb80a164a58c115866a109ef59322317
amrita150 <amritakataria515@gmail.com>
2023-09-03 23:18:05 +0530

    Count Sort

--- Sorting/CountSort.java
+++ Sorting/CountSort.java
@@ -33,3 +33,60 @@ public class CountSort {
         }
     }
 }
+
+/*
+//another way
+import java.util.*;
+
+public class Solution {
+
+    public static int[] sort(int n, int arr[]) {
+        if (n <= 0) {
+            return arr; // Return the array as is for empty or invalid input
+        }
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        // Find the maximum and minimum values in the array
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        // Create a count array with a size covering the range of values
+        int range = max - min + 1;
+        int count[] = new int[range];
+
+        // Store the count of each element
+        for (int i = 0; i < n; i++) {
+            count[arr[i] - min]++;
+        }
+
+        int j = 0;
+        for (int i = 0; i < range; i++) {
+            while (count[i] > 0) {
+                arr[j] = i + min; // Adjust the value back to its original range
+                j++;
+                count[i]--;
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        int arr[] = {4, 2, 2, 8, 3, 3};
+        arr = sort(n, arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+ */
